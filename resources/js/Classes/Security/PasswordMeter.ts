0dce7fa7973b75368f5db02db26206fc68cc10c4
drxgb <dr.xgb.rm2k@gmail.com>
2023-12-05 00:02:52 -0300

    Analisador de senha

--- resources/js/Classes/Security/PasswordMeter.ts
+++ resources/js/Classes/Security/PasswordMeter.ts
@@ -2,39 +2,60 @@ export class PasswordMeter {
 	/**
 	 * Verifica a força de uma senha.
 	 * @param password A senha a ser verificada
-	 * @returns Um objeto contendo a força da senha e a chave para mensagens
+	 * @returns O valor da força da senha
 	 */
-	public check(password: string): number {
-		let strength = 0;
-		let last = null;
+	public check(password?: string): number {
+		if (!password)
+			return 0;
 
-		for (const p of password) {
-			strength += this.compute(p, last);
-			last = p;
+		const data: any = this.analyzed(password);
+		const v = [
+			data.numbers > 0,
+			data.lowerCases > 0,
+			data.upperCases > 0,
+			data.symbols > 0,
+		];
+		const raise = (n: number) => v.filter(x => x === true).length * n;
+		let strength = 0;
+
+		strength += data.length;
+		strength += data.numbers * raise(2);
+		strength += data.lowerCases * raise(4);
+		strength += data.upperCases * raise(8);
+		strength += data.symbols * raise(16);
+
+		return strength;
+	}
+
+
+	/**
+	 * Cria um objeto baseado na senha, organizando o peso.
+	 * @param password A senha enviada
+	 * @returns Um conjunto de dados com informações de peso da senha
+	 */
+	private analyzed(password: string): object {
+		const result = {
+			length: 0,
+			lowerCases: 0,
+			upperCases: 0,
+			numbers: 0,
+			symbols: 0,
+		};
+		const set = new Set<string>(password);
+		const match = (regex: RegExp) => {
+			let p = '';
+			set.forEach(v => p += v);
+
+			const m = p.match(regex);
+			return m ? m.length : 0;
 		}
 
-		return strength + password.length;
-	}
+		result.length = set.size || 0;
+		result.lowerCases = match(/[a-z]/g);
+		result.upperCases = match(/[A-Z]/g);
+		result.numbers = match(/[0-9]/g);
+		result.symbols = match(/[^A-Za-z0-9]/g);
 
-
-	private compute(current: string, last?: string): number {
-		const c = this.getStrength(current);
-		const l = last !== null ? this.getStrength(last) : 0;
-
-		return Math.abs(c - l);
-	}
-
-
-
-	private getStrength(current: string): number {
-		const code = current.charCodeAt(0);
-
-		if (current.match(/[0-9]/))
-			return (code - 48 - 1) * 1;
-		if (current.match(/[a-z]/))
-			return (code - 97 - 1) * 2;
-		if (current.match(/[A-Z]/))
-			return (code - 65 - 1) * 4;
-		return (code - 33 - 1) * 8;
+		return result;
 	}
 }
--- resources/js/Components/PasswordExaminator.vue
+++ resources/js/Components/PasswordExaminator.vue
@@ -1,36 +1,61 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { PasswordMeter } from '@/Classes/Security/PasswordMeter.ts';
+import { PasswordMeter } from '@/Classes/Security/PasswordMeter';
 
 const props = defineProps<{
 	value: string,
 }>();
 
 const meter: PasswordMeter = new PasswordMeter;
-const password = computed<object>(() => {
-	const strength = meter.check(props.value);
-	let key = 'no_password';
+const strength: any = computed<number>(() => (meter.check(props.value) / 300) * 100);
+const message: any = computed<string>(() => {
+	const s = strength.value;
 
-	return { strength, key }
+	if (s === 0)
+		return 'no_password';
+	if (s <= 16)
+		return 'very_weak';
+	if (s <= 33)
+		return 'weak';
+	if (s <= 66)
+		return 'medium';
+	if (s <= 84)
+		return 'strong';
+
+	return 'very_strong';
 });
+
+function getBarColor(strength: number, background: boolean = false): string {
+	let color;
+	let intensity;
+
+	if (strength <= 33) {
+		color = 'red';
+		intensity = background ? 400 : 700;
+	} else if (strength <= 66) {
+		color = 'yellow';
+		intensity = background ? 100 : 400;
+	} else {
+		color = 'green';
+		intensity = background ? 300 : 500;
+	}
+
+	return `bg-${color}-${intensity}`;
+}
 </script>
 
 
 <template>
-	<div>
-		<div class="w-full h-2 flex gap-0.5 mb-2">
-			<div class="w-1/3 rounded-l-md bg-red-200">
-				<div />
-			</div>
-			<div class="w-1/3 bg-yellow-200">
-				<div />
-			</div>
-			<div class="w-1/3 rounded-r-md bg-green-200">
-				<div />
-			</div>
+	<div class="flex flex-col gap-2 items-center">
+		<div
+			class="w-full h-2 flex gap-0.5 overflow-hidden rounded-md"
+			:class="getBarColor(strength, true)">
+			<div
+				:class="[ 'h-2', getBarColor(strength) ]"
+				:style="`width: ${strength}%`" />
 		</div>
 		<span>
-			{{ $t(`passwords.${password.key}`) }}: {{ password.strength }}
+			{{ $t(`passwords.${message}`) }}
 		</span>
 	</div>
 </template>
